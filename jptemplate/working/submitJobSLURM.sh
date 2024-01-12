6418a6e47199b4b04e04d3a7825f311dbce1a5ac
Florian Jonas <florian.jonas@cern.ch>
2024-01-12 11:43:41 +0100

    making code docker compatible for submission on big clusters

--- jptemplate/working/submitJobSLURM.sh
+++ jptemplate/working/submitJobSLURM.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 #SBATCH -J jetphox
 #SBATCH -p long
-eval `alienv -w /software/flo/alice/sw/ --no-refresh load AliPhysics/latest lhapdf/latest`
-export LHAPDF_DATA_PATH=$LHAPDF_DATA_PATH:/cvmfs/sft.cern.ch/lcg/external/lhapdfsets/current:/alf/data/flo/pdfsets
+# eval `alienv -w /software/flo/alice/sw/ --no-refresh load AliPhysics/latest lhapdf/latest`
+# export LHAPDF_DATA_PATH=$LHAPDF_DATA_PATH:/cvmfs/sft.cern.ch/lcg/external/lhapdfsets/current:/alf/data/flo/pdfsets
 
-time ./$1
-exit $?
+# get current working directory
+CURRENTDIR=`pwd`
+
+# load jetphoxenv docker and run exe
+docker run -it --rm -v /alf/data/flo/PyPhox/:/home/PyPhox jetphoxenv:latest $CURRENTDIR/$1
+
+
+# time ./$1
+# exit $?
--- submit_jetphox.sh
+++ submit_jetphox.sh
@@ -12,4 +12,13 @@ ISO=2
 
 # export PERL5LIB=/alf/data/flo/BashPhox/jp1/working
 
-./run_jetphox --prefix Test_${DATE} --pdf1 nNNPDF30_nlo_as_0118_A208_Z82 --pdfmember1 0 --pdf2 nNNPDF30_nlo_as_0118_A208_Z82 --pdfmember2 0 --scaleis 1.0 --scalefs 1.0 --scalerenorm 1.0 --process dir --higherorder true --cme 5020 --maxrap '0.8' --minrap '-0.8' --isoconeradius 0.4 --isoenergy $ISO --numberofevents $NEVENTS --randomseed $RANDOMX --directcalculation $BORNBOX --jetrapmin '-5.8' --jetrapmax '5.8' --skipcopy false
+# 1. run docker container jetphoxenv:latest and mount directory  /alf/data/flo/PyPhox/ under /PyPhox
+# 2. go into /PyPhox and run ./run_jetphox
+
+RUNOPTIONS = "--prefix Test_${DATE} --pdf1 nNNPDF30_nlo_as_0118_A208_Z82 --pdfmember1 0 --pdf2 nNNPDF30_nlo_as_0118_A208_Z82 --pdfmember2 0 --scaleis 1.0 --scalefs 1.0 --scalerenorm 1.0 --process dir --higherorder true --cme 5020 --maxrap '0.8' --minrap '-0.8' --isoconeradius 0.4 --isoenergy $ISO --numberofevents $NEVENTS --randomseed $RANDOMX --directcalculation $BORNBOX --jetrapmin '-5.8' --jetrapmax '5.8' --skipcopy false"
+
+
+# build all files needed (inside docker)
+docker run -it --rm -v /alf/data/flo/PyPhox/:/home/PyPhox jetphoxenv:latest run_jetphox build $RUNOPTIONS
+# submit job to slurm (outside docker, since docker container does not know sbatch)
+./run_jetphox submit $RUNOPTIONS

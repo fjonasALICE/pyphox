2b2c251523a04a7eb6ba05e112c21b6ec1d60875
Florian Jonas <florian.jonas@cern.ch>
2024-01-16 11:21:09 +0100

    added calculation of frag contribution

--- build_and_submit.sh
+++ build_and_submit.sh
@@ -5,11 +5,18 @@ NEVENTS=5000000
 BORNBOX=2
 ISO=2
 
-RUNOPTIONS="--prefix Test_${DATE} --pdf1 nNNPDF30_nlo_as_0118_A208_Z82 --pdfmember1 0 --pdf2 nNNPDF30_nlo_as_0118_A208_Z82 --pdfmember2 0 --scaleis 1.0 --scalefs 1.0 --scalerenorm 1.0 --process dir --higherorder true --cme 5020 --maxrap 0.8 --minrap -0.8 --isoconeradius 0.4 --isoenergy $ISO --numberofevents $NEVENTS --randomseed $RANDOMX --directcalculation $BORNBOX --jetrapmin -5.8 --jetrapmax 5.8 --skipcopy false"
+RUNOPTIONS="--prefix Test_${DATE} --pdf1 nNNPDF30_nlo_as_0118_A208_Z82 --pdfmember1 0 --pdf2 nNNPDF30_nlo_as_0118_A208_Z82 --pdfmember2 0 --scaleis 1.0 --scalefs 1.0 --scalerenorm 1.0 --higherorder true --cme 5020 --maxrap 0.8 --minrap -0.8 --isoconeradius 0.4 --isoenergy $ISO --numberofevents $NEVENTS --randomseed $RANDOMX --directcalculation $BORNBOX --jetrapmin -5.8 --jetrapmax 5.8 --skipcopy false"
 
-buildjobid=$(sbatch build_jetphox_perlmutter.sh $RUNOPTIONS)
+# build and submit job to slurm
 
-# submit subit_jetphox_perlmutter.sh after build_jetphox_perlmutter.sh has finished
-sbatch --dependency=afterok:${buildjobid##* } submit_jetphox_perlmutter.sh $RUNOPTIONS
+# dir
+buildjobdirid=$(sbatch build_jetphox_perlmutter.sh $RUNOPTIONS --process dir)
+sbatch --dependency=afterok:${buildjobdirid##* } submit_jetphox_perlmutter.sh $RUNOPTIONS --process dir
+
+# frag
+# build frag jobID only if dir jobID is done
+buildjobfragid=$(sbatch --dependency=afterok:${buildjobdirid##* } build_jetphox_perlmutter.sh $RUNOPTIONS --process onef)
+sbatch --dependency=afterok:${buildjobfragid##* } submit_jetphox_perlmutter.sh $RUNOPTIONS --process onef
+
 
 

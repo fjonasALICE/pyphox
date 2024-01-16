d674042f445323c4ce501781d21716e2730dd1a8
Florian Jonas <florian.jonas@cern.ch>
2024-01-16 16:01:40 +0100

    further fixed pdf unc

--- build_and_submit_pdfunc.sh
+++ build_and_submit_pdfunc.sh
@@ -18,22 +18,22 @@ cd $SCRATCHPATH
 buildjobdirid=0
 for i in {0..101}
 do
-RUNOPTIONS="--prefix Test_${DATE} --pdf1 NNPDF40_nnlo_as_01180 --pdfmember1 ${i} --pdf2 NNPDF40_nnlo_as_01180 --pdfmember2 ${i} --scaleis 1.0 --scalefs 1.0 --scalerenorm 1.0 --higherorder true --cme 5020 --maxrap 0.8 --minrap -0.8 --isoconeradius 0.4 --isoenergy $ISO --numberofevents $NEVENTS --randomseed $RANDOMX --directcalculation $BORNBOX --jetrapmin -5.8 --jetrapmax 5.8 --skipcopy false"
+RUNOPTIONS="--prefix Dhruv_${DATE} --pdf1 NNPDF40_nnlo_as_01180 --pdfmember1 ${i} --pdf2 NNPDF40_nnlo_as_01180 --pdfmember2 ${i} --scaleis 1.0 --scalefs 1.0 --scalerenorm 1.0 --higherorder true --cme 5020 --maxrap 0.8 --minrap -0.8 --isoconeradius 0.4 --isoenergy $ISO --numberofevents $NEVENTS --randomseed $RANDOMX --directcalculation $BORNBOX --jetrapmin -5.8 --jetrapmax 5.8"
 
 # build and submit job to slurm
 # dir
 if [ $i -eq 0 ]
 then
-buildjobdirid=$(sbatch build_jetphox_perlmutter.sh $RUNOPTIONS --process dir)
+buildjobdirid=$(sbatch build_jetphox_perlmutter.sh $RUNOPTIONS --process dir --skipcopy false)
 else
-buildjobdirid=$(sbatch --dependency=afterok:${buildjobdirid##* } build_jetphox_perlmutter.sh $RUNOPTIONS --process dir)
+buildjobdirid=$(sbatch --dependency=afterok:${buildjobdirid##* } build_jetphox_perlmutter.sh $RUNOPTIONS --process dir --skipcopy true)
 fi
 
-sbatch --dependency=afterok:${buildjobdirid##* } submit_jetphox_perlmutter.sh $RUNOPTIONS --process dir
+sbatch --dependency=afterok:${buildjobdirid##* } submit_jetphox_perlmutter.sh $RUNOPTIONS --process dir --skipcopy true
 
 # frag
-buildjobfragid=$(sbatch --dependency=afterok:${buildjobdirid##* } build_jetphox_perlmutter.sh $RUNOPTIONS --process onef)
-sbatch --dependency=afterok:${buildjobfragid##* } submit_jetphox_perlmutter.sh $RUNOPTIONS --process onef
+buildjobfragid=$(sbatch --dependency=afterok:${buildjobdirid##* } build_jetphox_perlmutter.sh $RUNOPTIONS --process onef --skipcopy true)
+sbatch --dependency=afterok:${buildjobfragid##* } submit_jetphox_perlmutter.sh $RUNOPTIONS --process onef -skipcopy true
 done
 
 cd $LOCALPATH

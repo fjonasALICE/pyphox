292652f4aa9df630f39d2ec519b41511cb111bcb
Florian Jonas <florian.jonas@cern.ch>
2024-01-16 15:24:21 +0100

    bugfix pdf unc

--- build_and_submit_pdfunc.sh
+++ build_and_submit_pdfunc.sh
@@ -15,13 +15,20 @@ cd $SCRATCHPATH
 
 # do PDF uncertainties for pp
 # loop over all PDF members
+buildjobdirid=0
 for i in {0..101}
 do
 RUNOPTIONS="--prefix Test_${DATE} --pdf1 NNPDF40_nnlo_as_01180 --pdfmember1 ${i} --pdf2 NNPDF40_nnlo_as_01180 --pdfmember2 ${i} --scaleis 1.0 --scalefs 1.0 --scalerenorm 1.0 --higherorder true --cme 5020 --maxrap 0.8 --minrap -0.8 --isoconeradius 0.4 --isoenergy $ISO --numberofevents $NEVENTS --randomseed $RANDOMX --directcalculation $BORNBOX --jetrapmin -5.8 --jetrapmax 5.8 --skipcopy false"
 
 # build and submit job to slurm
 # dir
+if [ $i -eq 0 ]
+then
 buildjobdirid=$(sbatch build_jetphox_perlmutter.sh $RUNOPTIONS --process dir)
+else
+buildjobdirid=$(sbatch --dependency=afterok:${buildjobdirid##* } build_jetphox_perlmutter.sh $RUNOPTIONS --process dir)
+fi
+
 sbatch --dependency=afterok:${buildjobdirid##* } submit_jetphox_perlmutter.sh $RUNOPTIONS --process dir
 
 # frag

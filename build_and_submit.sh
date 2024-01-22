046d7776815dc761cfe8068506d86ff44542f19a
Florian Jonas <florian.jonas@cern.ch>
2024-01-22 13:55:02 +0100

    changign from copy to rsync

--- build_and_submit.sh
+++ build_and_submit.sh
@@ -9,9 +9,8 @@ LOCALPATH=${HOME}/pyphox/
 SCRATCHPATH=${PSCRATCH}/pyphox/
 
 # copy everything to scratch storage
-cp -r $LOCALPATH $SCRATCHPATH
+rsync -av --progress $LOCALPATH $SCRATCHPATH
 cd $SCRATCHPATH
-
 
 RUNOPTIONS="--prefix Test_${DATE} --pdf1 nNNPDF30_nlo_as_0118_A208_Z82 --pdfmember1 0 --pdf2 nNNPDF30_nlo_as_0118_A208_Z82 --pdfmember2 0 --scaleis 1.0 --scalefs 1.0 --scalerenorm 1.0 --higherorder true --cme 5020 --maxrap 0.8 --minrap -0.8 --isoconeradius 0.4 --isoenergy $ISO --numberofevents $NEVENTS --randomseed $RANDOMX --directcalculation $BORNBOX --jetrapmin -5.8 --jetrapmax 5.8 --skipcopy false"
 
